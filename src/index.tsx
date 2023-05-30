2dc5a1fd208cf5e9328d5d3bf1c9018d81463ee2
Jan Eike Suchard <jan@suchard.cloud>
2023-05-30 11:09:17 +0200

    🚧 adding authentication to the routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BaseApp from "./App";
 import reportWebVitals from './reportWebVitals';
+import {AuthProvider} from "react-oidc-context";
+import {User} from "oidc-client-ts";
+import env from "./env"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {LoginPage} from "./pages/login/login";
+
+const oidcConfig = {
+    authority: env.OIDC_AUTHORITY,
+    client_id: env.OIDC_CLIENT_ID,
+    redirect_uri: window.location.protocol + "//" + window.location.host + "/callback",
+    scope: "openid profile email",
+    onSigninCallback: (user: User | void) => {
+        console.log(user)
+        console.log(window.location)
+        window.history.replaceState({}, document.title, window.location.pathname);
+        window.location.replace("/")
+    },
+
+
+}
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
 root.render(
-    <React.StrictMode>
-        <BaseApp />
-    </React.StrictMode>
+    <AuthProvider {...oidcConfig}>
+        <Router>
+            <Routes>
+                <Route path={"/"} element={<BaseApp />}/>
+                <Route path={"/login"} element={<LoginPage />}/>
+            </Routes>
+        </Router>
+    </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
