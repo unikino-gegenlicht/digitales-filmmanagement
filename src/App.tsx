f9509f3c0bce2faab3d852fe3780ba32cc8a1fdb
Jan Eike Suchard <jan@suchard.cloud>
2023-05-30 11:05:21 +0200

    🚧 extending base app with authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 // FIXME: replace later with actual application code or remove file
-import React, {MouseEventHandler} from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import {Button} from "react-bulma-components";
+import {AuthContextProps, withAuth} from "react-oidc-context";
+import {Navigate} from "react-router-dom";
 
-function BaseApp() {
-    return (
-        <div className={"content"}>
-            <h2 className={"title is-size-1 has-text-centered has-text-primary"}>Digitales Filmmanagement</h2>
-            <div className={"box m-3"}>
-                <p>Hier entsteht aktuell das digitale Filmmanagement des Unikinos GEGENLICHT</p>
-                <p className={"is-info has-text-centered is-size-5"}>
-                    Diese Seite wird gelöscht, sobald das Projekt seinen &alpha;-Status erreicht hat.
-                </p>
-                <Button color={"primary"}>Klick me!</Button>
-            </div>
-        </div>
+class BaseApp extends Component<any, any> {
+    render() {
+        // access the auth properties
+        let auth: AuthContextProps = this.props?.auth
+        console.log(auth)
+        // now check if the authentication is loading and suspend the page while it loads
+        if (auth.isLoading) {
+            return (<div></div>);
+        }
+        // now check if the user is authenticated
+        if (auth.isAuthenticated) {
+            return (
+                <div className={"content"}>
+                    <h2 className={"title is-size-1 has-text-centered has-text-primary"}>Digitales Filmmanagement</h2>
+                    <div className={"box m-3"}>
+                        <p>Hier entsteht aktuell das digitale Filmmanagement des Unikinos GEGENLICHT</p>
+                        <p className={"has-text-info has-text-centered is-size-5"}>
+                            Diese Seite wird gelöscht, sobald das Projekt seinen &alpha;-Status erreicht hat.
+                        </p>
+                        <Button color={"primary"}>Klick me!</Button>
+                    </div>
+                </div>
+            );
+        } else {
+            return <Navigate to={"/login"}></Navigate>
+        }
 
-    );
+    }
 }
 
-export default BaseApp;
+export default withAuth(BaseApp);
