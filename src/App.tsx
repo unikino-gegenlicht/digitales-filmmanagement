9ac733b4bf01c009d3837e33559f23ac12f89ffc
Jan Eike Suchard <jan@suchard.cloud>
2023-05-30 08:18:57 +0200

    🔥 removing own written bulma react components in favor of `react-bulma-components`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 // FIXME: replace later with actual application code or remove file
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEventHandler} from 'react';
 import './App.scss';
-import {TextColor} from "./bulma/Colors";
+import {Button} from "react-bulma-components";
 
 function BaseApp() {
     return (
@@ -10,9 +9,10 @@ function BaseApp() {
             <h2 className={"title is-size-1 has-text-centered has-text-primary"}>Digitales Filmmanagement</h2>
             <div className={"box m-3"}>
                 <p>Hier entsteht aktuell das digitale Filmmanagement des Unikinos GEGENLICHT</p>
-                <p className={TextColor.Info + " has-text-centered is-size-5"}>
+                <p className={"is-info has-text-centered is-size-5"}>
                     Diese Seite wird gelöscht, sobald das Projekt seinen &alpha;-Status erreicht hat.
                 </p>
+                <Button color={"primary"}>Klick me!</Button>
             </div>
         </div>
 
